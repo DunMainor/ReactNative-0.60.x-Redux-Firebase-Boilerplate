4a18eddfa34a6c3d7fc119b03fa7cf83eb0183f3
DunMainor <DanMainor@outlook.com>
2019-11-04 11:36:26 +0300

    ADD: SplashScreen show on app launch

--- android/app/src/main/java/com/zaidiapps/boilerplateapp/MainActivity.java
+++ android/app/src/main/java/com/zaidiapps/boilerplateapp/MainActivity.java
@@ -1,9 +1,13 @@
 package com.zaidiapps.boilerplateapp;
 
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
 import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
+
+// Splashscreen
+import org.devio.rn.splashscreen.SplashScreen;
 
 public class MainActivity extends ReactActivity {
     @Override
@@ -12,12 +16,18 @@ public class MainActivity extends ReactActivity {
     }
 
     @Override
-  protected ReactActivityDelegate createReactActivityDelegate() {
-    return new ReactActivityDelegate(this, getMainComponentName()) {
-      @Override
-      protected ReactRootView createRootView() {
-       return new RNGestureHandlerEnabledRootView(MainActivity.this);
-      }
-    };
-  }
+    protected void onCreate(Bundle savedInstanceState) {
+      SplashScreen.show(this, R.style.SplashScreenTheme); // show splashscreen
+      super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected ReactActivityDelegate createReactActivityDelegate() {
+      return new ReactActivityDelegate(this, getMainComponentName()) {
+        @Override
+        protected ReactRootView createRootView() {
+        return new RNGestureHandlerEnabledRootView(MainActivity.this);
+        }
+      };
+    }
 }
