858ed50d47813bf129a59166af29ab8f08b68a6b
DunMainor <DanMainor@outlook.com>
2019-11-05 13:12:18 +0300

    ADD: Reactotron configs and initialization

--- src/App/Container/RootRedux.js
+++ src/App/Container/RootRedux.js
@@ -2,16 +2,30 @@ import React from 'react'
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
-
+import { composeWithDevTools } from 'redux-devtools-extension';
 import { persistStore } from 'redux-persist'
 import { PersistGate } from 'redux-persist/es/integration/react'
 import * as ActionTypes from '@actions/ActionTypes'
 import reducers from '@reducers'
 import AppContainer from './AppContainer'
-
+import { reactotronRedux } from 'reactotron-redux'
+import Reactotron from 'reactotron-react-native'
+import AsyncStorage from '@react-native-community/async-storage'
 
 const middleware = [thunk]
-const store = compose(applyMiddleware(...middleware))(createStore)(reducers)
+let composer
+if (__DEV__) {
+    Reactotron.configure({ port: 9090 })
+        .setAsyncStorageHandler(AsyncStorage)
+        .useReactNative()
+        .use(reactotronRedux())
+        .connect()
+        .clear()
+        composer = composeWithDevTools(applyMiddleware(...middleware),Reactotron.createEnhancer())
+} else {
+    composer = compose(applyMiddleware(...middleware))
+}
+const store = createStore(reducers, composer)
 const persistor = persistStore(store)
 
 export default class RootRedux extends React.Component {
